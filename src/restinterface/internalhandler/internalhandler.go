568cddf4a60f2ce159f70dae40035e93d0d577b8
tdrozdovsky <45031429+tdrozdovsky@users.noreply.github.com>
2020-11-04 13:54:04 +0200

    Fix copyright date (#157)
    Signed-off-by: Taras Drozdovskyi <t.drozdovsky@samsung.com>
    
    Revision of the copyright statement for updated/created 17 files from the latest Coconut release.

--- src/restinterface/internalhandler/internalhandler.go
+++ src/restinterface/internalhandler/internalhandler.go
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2020 Samsung Electronics All Rights Reserved.
+ * Copyright 2019-2020 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,7 +100,6 @@ func init() {
 			Pattern:     "/api/v1/discoverymgr/orchestrationinfo",
 			HandlerFunc: handler.APIV1DiscoverymgrOrchestrationInfoGet,
 		},
-
 	}
 }
 
