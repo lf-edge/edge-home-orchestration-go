f9bbcc3e8dd21c62a5e62b62dcf82813ec8d8e0a
Sunchit Sharma <sun.sharma@samsung.com>
2020-12-23 13:18:57 +0530

    Review Changes 2
    Signed-off-by: Sunchit Sharma <sun.sharma@samsung.com>

--- src/controller/storagemgr/storagedriver/storagehandler_test.go
+++ src/controller/storagemgr/storagedriver/storagehandler_test.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2020 Samsung Electronics Co., Ltd All Rights Reserved.
 //
+// Copyright (c) 2020 Samsung Electronics Co., Ltd All Rights Reserved.
 // Copyright (c) 2019 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
