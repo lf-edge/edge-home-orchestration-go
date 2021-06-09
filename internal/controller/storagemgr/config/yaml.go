92f2015d76380d2d3d8a9646e1ffc0c23b84f314
Taewan Kim <t25.kim@samsung.com>
2021-06-09 16:52:04 +0900

    [DataStorage] Add SetYaml function for DataStorage configuration (#311)
    Signed-off-by: Taewan Kim <t25.kim@samsung.com>
    
    - This PR is intended to create DataStorage's configuration file, such as datastorage-device.yaml dynamically.
    - Related #302

---
+++ internal/controller/storagemgr/config/yaml.go
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright 2021 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+)
+
+type Yaml struct {
+	Name            string           `yaml:"name"`
+	Manufacturer    string           `yaml:"manufacturer"`
+	Model           string           `yaml:"model"`
+	Labels          []string         `yaml:"labels,omitempty"`
+	Description     string           `yaml:"description,omitempty"`
+	DeviceResources []DeviceResource `yaml:"deviceResources,omitempty"`
+}
+
+type DeviceResource struct {
+	Name        string   `yaml:"name"`
+	Description string   `yaml:"description,omitempty"`
+	Properties  Property `yaml:"properties"`
+}
+
+type Property struct {
+	Value PropertyDetail `yaml:"value,flow"`
+	Units PropertyDetail `yaml:"units,flow"`
+}
+
+type PropertyDetail struct {
+	Type      string `yaml:"type"`
+	ReadWrite string `yaml:"readWrite"`
+	MediaType string `yaml:"mediaType,omitempty"`
+}
+
+var (
+	yamlInfo Yaml
+)
+
+func SetYaml(name, manufac, model, desc string, labels []string, resources []DeviceResource) {
+	yamlInfo = Yaml{
+		Name: name,
+		Manufacturer: manufac,
+		Model: model,
+		Labels: labels,
+		Description: desc,
+		DeviceResources: resources}
+}
+
+func YamlMarshal() (b []byte, err error) {
+	return yaml.Marshal(yamlInfo)
+}
---
+++ internal/controller/storagemgr/config/yaml_test.go
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright 2021 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+package config
+
+import (
+	"testing"
+)
+
+var (
+	testName           = "datastorage"
+	testManufacture    = "Home Edge"
+	testModel          = "Home Edge"
+	testLabel          = []string{"rest", "json", "numeric", "float", "int"}
+	testDescription    = "REST Device"
+	testPropertyJson   = Property{
+				Value:PropertyDetail{
+					Type:"String",
+					ReadWrite:"RW",
+					MediaType:"application/json"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testPropertyInt    = Property{
+				Value:PropertyDetail{
+					Type:"Int64",
+					ReadWrite:"RW",
+					MediaType:"text/plain"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testPropertyFloat  = Property{
+				Value:PropertyDetail{
+					Type:"Float64",
+					ReadWrite:"RW",
+					MediaType:"text/plain"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testPropertyJpeg   = Property{
+				Value:PropertyDetail{
+					Type:"Binary",
+					ReadWrite:"RW",
+					MediaType:"image/jpeg"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testPropertyPng    = Property{
+				Value:PropertyDetail{
+					Type:"Binary",
+					ReadWrite:"RW",
+					MediaType:"image/png"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testPropertyString = Property{
+				Value:PropertyDetail{
+					Type:"String",
+					ReadWrite:"RW",
+					MediaType:"text/plain"},
+				Units:PropertyDetail{
+					Type:"String",
+					ReadWrite:"R"}}
+	testResource       = []DeviceResource{
+				DeviceResource{
+					Name:"json",
+					Description:"json",
+					Properties:testPropertyJson},
+				DeviceResource{
+					Name:"int",
+					Properties:testPropertyInt},
+				DeviceResource{
+					Name:"float",
+					Description:"float",
+					Properties:testPropertyFloat},
+				DeviceResource{
+					Name:"jpeg",
+					Description:"jpeg",
+					Properties:testPropertyJpeg},
+				DeviceResource{
+					Name:"png",
+					Description:"png",
+					Properties:testPropertyPng},
+				DeviceResource{
+					Name:"string",
+					Description:"string",
+					Properties:testPropertyString}}
+)
+
+func TestYaml(t *testing.T) {
+	SetYaml(testName, testManufacture, testModel, testDescription, testLabel, testResource)
+
+	b, err := YamlMarshal()
+	if err != nil {
+		t.Fatal("Unexpected Error")
+	}
+
+	log.Println(string(b))
+}
